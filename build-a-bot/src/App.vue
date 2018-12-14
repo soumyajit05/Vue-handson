1743eff174123997197f98a6fcddc07b915e268f
soumyajit05 <ray.soumyajit24@gmail.com>
2018-12-13 23:50:42 -0500

    basic bind

---
+++ build-a-bot/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HomePage msg="Welcome to Your Vue.js App"/> -->
+    <RobotBuilder/>
+  </div>
+</template>
+
+<script>
+// import HomePage from './home/HomePage.vue';
+import RobotBuilder from './build/RobotBuilder.vue';
+
+export default {
+  name: 'app',
+  components: {
+    RobotBuilder,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ readme.md
@@ -0,0 +1,4 @@
+Vue Cli
+
+0. Install vue cli globally - npm install -g @vue/cli
+1. Create - vue create project_name
